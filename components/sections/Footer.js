3dbb3b37819fe47294904180034e6120d5e0912f
JP Ungaretti <hello@jungaretti.com>
2021-01-31 20:42:08 -0600

    Create Footer component

--- components/sections/Footer.js
+++ components/sections/Footer.js
@@ -1,24 +1,22 @@
-import { Box, Divider, Link, Text } from "@chakra-ui/react";
+import { Divider, Text, Link } from "@chakra-ui/react";
 import styles from "../../styles/Footer.module.css";
 
 export default function Footer() {
   return (
-    <Box as="footer" className={styles.container}>
+    <footer className={styles.container}>
       <Divider className={styles.divider} />
-      <Box>
-        <Text>
-          Crafted with{" "}
-          <Link href="https://nextjs.org/" isExternal>
-            Next.js
-          </Link>{" "}
-          and{" "}
-          <Link href="https://chakra-ui.com/" isExternal>
-            Chakra UI
-          </Link>{" "}
-          in Madison, Wisconsin.
-        </Text>
-        <Text>MIT License © 2021 James Ungaretti.</Text>
-      </Box>
-    </Box>
+      <Text>
+        Crafted with{" "}
+        <Link href="https://nextjs.org/" isExternal>
+          Next.js
+        </Link>{" "}
+        and{" "}
+        <Link href="https://chakra-ui.com/" isExternal>
+          Chakra UI
+        </Link>{" "}
+        in Madison, Wisconsin.
+      </Text>
+      <Text>MIT License © 2021 James Ungaretti.</Text>
+    </footer>
   );
 }
