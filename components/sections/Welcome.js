dafa314205756c085fe6257be0f5529f29553800
JP Ungaretti <hello@jungaretti.com>
2021-01-31 21:53:00 -0600

    Add some style to Welcome

--- components/sections/Welcome.js
+++ components/sections/Welcome.js
@@ -4,14 +4,28 @@ import SocialLink from "../SocialLink";
 
 export default function Welcome() {
   return (
-    <Box as="section" marginY="20px">
-      <Heading size="xl">Ciao, I'm JP</Heading>
-      <Text fontSize="lg">Student / Developer</Text>
-      <SocialLink
-        name="GitHub"
-        link="https://github.com/jungaretti/"
-        icon={<FaGithub />}
-      />
+    <Box
+      as="section"
+      marginY="8px"
+      paddingX="12px"
+      bgGradient="linear(to-r, cyan.800, blue.900)"
+      borderRadius="md"
+    >
+      <Box paddingTop="3rem">
+        <Heading size="xl" color="white">
+          Ciao, I'm JP
+        </Heading>
+        <Text fontSize="md" color="white">
+          Student / Developer
+        </Text>
+      </Box>
+      <Box marginTop="16px" paddingBottom="2rem">
+        <SocialLink
+          name="GitHub"
+          link="https://github.com/jungaretti/"
+          icon={<FaGithub />}
+        />
+      </Box>
     </Box>
   );
 }
