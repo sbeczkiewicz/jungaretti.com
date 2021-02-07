8b3e47a8b0b1d4fbb2173fef2b64905ba85f40d6
JP Ungaretti <hello@jungaretti.com>
2021-02-06 23:56:54 -0600

    Style Welcome section

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,14 @@
+import { Box, Heading } from "@chakra-ui/react";
+
 export default function Welcome() {
   return (
-    <section>
-      <p>Ciao, I'm JP!</p>
+    <Box as="section" backgroundColor="black" color="white">
+      <Heading>Ciao, I'm JP!</Heading>
       <p>
         I'm a developer studying computer science and Italian. I love to swim,
         play board games, mess around with Linux, and visit national parks.
         Welcome to my corner of the Internet!
       </p>
-    </section>
+    </Box>
   );
 }
