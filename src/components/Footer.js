5bbe63a8a11a74d0bab550f8e1501f538f23e878
JP Ungaretti <hello@jungaretti.com>
2021-02-10 19:10:51 -0600

    Add links to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,12 @@
-import { Box, Divider, Heading, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  Heading,
+  Link as ExternalLink,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import Link from "next/link";
 
 function FooterColumn({ title, children }) {
   return (
@@ -15,22 +23,31 @@ export default function Footer() {
   return (
     <Box as="footer" marginY={4}>
       <Divider marginBottom={4} />
-      <Stack spacing={4} direction={["column", null, "row"]}>
+      <Stack spacing={8} direction={["column", null, "row"]}>
         <FooterColumn title="Here">
-          <span>Blog</span>
-          <span>Home</span>
-          <span>Contact</span>
+          <Link href="/">Home</Link>
+          {/* <Link href="/blog">Blog</Link> */}
+          {/* <Link href="/contact">Contact</Link> */}
         </FooterColumn>
         <FooterColumn title="There">
-          <span>GitHub</span>
-          <span>YouTube</span>
-          <span>Twitter</span>
+          <ExternalLink href="https://github.com/jungaretti/" isExternal>
+            GitHub
+          </ExternalLink>
+          <ExternalLink href="https://twitter.com/jpungaretti" isExternal>
+            Twitter
+          </ExternalLink>
+          <ExternalLink
+            href="https://www.youtube.com/user/jungaretti"
+            isExternal
+          >
+            YouTube
+          </ExternalLink>
         </FooterColumn>
         <FooterColumn title="Anywhere">
-          <span>
+          <Text>
             Crafted with Next.js and Chakra UI in Madison, Wisconsin. Inspired
             by Lee Robinson, Griko Nibras, and Alec Lomas.
-          </span>
+          </Text>
         </FooterColumn>
       </Stack>
     </Box>
