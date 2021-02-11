a816731860f5f0117f7a9d91accd211b94fe237f
JP Ungaretti <hello@jungaretti.com>
2021-02-10 18:48:32 -0600

    Use Box instead of header

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,17 +2,20 @@ import { Box, Container, Heading } from "@chakra-ui/react";
 
 export default function Welcome() {
   return (
-    <section>
-      <Box py={10} backgroundColor="black" color="white">
-        <Container>
-          <Heading>Ciao, I'm JP!</Heading>
-          <p>
-            I'm a developer studying computer science and Italian. I love to
-            swim, play board games, mess around with Linux, and visit national
-            parks. Welcome to my corner of the Internet!
-          </p>
-        </Container>
-      </Box>
-    </section>
+    <Box
+      as="section"
+      paddingY={[8, null, 12]}
+      backgroundColor="black"
+      color="white"
+    >
+      <Container>
+        <Heading>Ciao, I'm JP!</Heading>
+        <p>
+          I'm a developer studying computer science and Italian. I love to swim,
+          play board games, mess around with Linux, and visit national parks.
+          Welcome to my corner of the Internet!
+        </p>
+      </Container>
+    </Box>
   );
 }
