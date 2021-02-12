bd665752ce4ee98bbdc88304622a68c17dac84d5
JP Ungaretti <hello@jungaretti.com>
2021-02-12 15:50:29 -0600

    Apply flex style to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,30 @@
+function FooterColumn({ title, children }) {
+  return (
+    <div className="ml-2 first:ml-0">
+      <h5 className="font-bold">{title}</h5>
+      {children}
+    </div>
+  );
+}
+
 export default function Footer() {
   return (
     <footer>
-      <hr />
-      <p>
-        Crafted with Next.js in Madison, Wisconsin. Inspired by Lee Robinson,
-        Griko Nibras, and Alec Lomas.
-      </p>
+      <hr className="mb-1" />
+      <div className="flex">
+        <FooterColumn title="Here">
+          <span>Home</span>
+        </FooterColumn>
+        <FooterColumn title="There">
+          <span>GitHub</span>
+        </FooterColumn>
+        <FooterColumn title="Everywhere">
+          <p>
+            Crafted with Next.js in Madison, Wisconsin. Inspired by Lee
+            Robinson, Griko Nibras, and Alec Lomas.
+          </p>
+        </FooterColumn>
+      </div>
     </footer>
   );
 }
