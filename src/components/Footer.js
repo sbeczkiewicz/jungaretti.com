03a72d9ac03f0464ea767c8d1984f43df149877e
JP Ungaretti <hello@jungaretti.com>
2021-02-12 15:54:08 -0600

    Make Footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function FooterColumn({ title, children }) {
   return (
-    <div className="ml-2 first:ml-0">
+    <div className="sm:ml-2 sm:first:ml-0">
       <h5 className="font-bold">{title}</h5>
       {children}
     </div>
@@ -11,7 +11,7 @@ export default function Footer() {
   return (
     <footer>
       <hr className="mb-1" />
-      <div className="flex">
+      <div className="sm:flex">
         <FooterColumn title="Here">
           <span>Home</span>
         </FooterColumn>
@@ -20,8 +20,8 @@ export default function Footer() {
         </FooterColumn>
         <FooterColumn title="Everywhere">
           <p>
-            Crafted with Next.js in Madison, Wisconsin. Inspired by Lee
-            Robinson, Griko Nibras, and Alec Lomas.
+            Crafted with Next.js and Tailwind CSS in Madison, Wisconsin.
+            Inspired by Lee Robinson, Griko Nibras, and Alec Lomas.
           </p>
         </FooterColumn>
       </div>
