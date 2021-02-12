f660acef916552d67db3a8392c7420bc87ddae09
JP Ungaretti <hello@jungaretti.com>
2021-02-11 18:33:16 -0600

    Fix footer tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <header>
+    <footer>
       <span>
         Crafted with Next.js and Chakra UI in Madison, Wisconsin. Inspired by
         Lee Robinson, Griko Nibras, and Alec Lomas.
       </span>
-    </header>
+    </footer>
   );
 }
