88bb0e5d1e04bdab56ae776680d80b3e17007935
JP Ungaretti <hello@jungaretti.com>
2021-02-12 15:18:00 -0600

    Quick changes to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 export default function Footer() {
   return (
     <footer>
-      <span>
-        Crafted with Next.js and Chakra UI in Madison, Wisconsin. Inspired by
-        Lee Robinson, Griko Nibras, and Alec Lomas.
-      </span>
+      <hr />
+      <p>
+        Crafted with Next.js in Madison, Wisconsin. Inspired by Lee Robinson,
+        Griko Nibras, and Alec Lomas.
+      </p>
     </footer>
   );
 }
