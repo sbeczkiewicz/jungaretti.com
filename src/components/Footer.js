050ee9f1201a942f903a5598ede3c538b696cc11
JP Ungaretti <hello@jungaretti.com>
2021-02-13 17:00:11 -0600

    Add links to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 function FooterColumn({ title, children }) {
   return (
     <div>
@@ -13,15 +15,36 @@ export default function Footer() {
       <hr />
       <div className="mt-4 space-y-2 sm:space-y-0 sm:flex sm:space-x-4">
         <FooterColumn title="Here">
-          <span>Home</span>
+          <Link href="/">Home</Link>
         </FooterColumn>
         <FooterColumn title="There">
-          <span>GitHub</span>
+          <a href="https://github.com/jungaretti/" target="_blank">
+            GitHub
+          </a>
         </FooterColumn>
         <FooterColumn title="Everywhere">
           <p>
-            Crafted with Next.js and Tailwind CSS in Madison, Wisconsin.
-            Inspired by Lee Robinson, Griko Nibras, and Alec Lomas.
+            Crafted with{" "}
+            <a href="https://nextjs.org/" target="_blank">
+              Next.js
+            </a>{" "}
+            and{" "}
+            <a href="https://tailwindcss.com/" target="_blank">
+              Tailwind CSS
+            </a>{" "}
+            in Madison, Wisconsin. Inspired by{" "}
+            <a href="https://leerob.io/" target="_blank">
+              Lee Robinson
+            </a>
+            ,{" "}
+            <a href="https://griko.id/" target="_blank">
+              Griko Nibras
+            </a>
+            , and{" "}
+            <a href="https://lowmess.com/" target="_blank">
+              Alec Lomas
+            </a>
+            .
           </p>
         </FooterColumn>
       </div>
