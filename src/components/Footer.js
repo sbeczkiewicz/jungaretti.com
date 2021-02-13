ca01ea23eb0488f1bfdbcc7cbf5c61f13643dae1
JP Ungaretti <hello@jungaretti.com>
2021-02-13 17:45:30 -0600

    Add underline to Footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,33 +15,47 @@ export default function Footer() {
       <hr />
       <div className="mt-4 space-y-2 sm:space-y-0 sm:flex sm:space-x-4">
         <FooterColumn title="Here">
-          <Link href="/">Home</Link>
+          <span className="underline">
+            <Link href="/">Home</Link>
+          </span>
         </FooterColumn>
         <FooterColumn title="There">
-          <a href="https://github.com/jungaretti/" target="_blank">
+          <a
+            href="https://github.com/jungaretti/"
+            target="_blank"
+            className="underline"
+          >
             GitHub
           </a>
         </FooterColumn>
         <FooterColumn title="Everywhere">
           <p>
             Crafted with{" "}
-            <a href="https://nextjs.org/" target="_blank">
+            <a href="https://nextjs.org/" target="_blank" className="underline">
               Next.js
             </a>{" "}
             and{" "}
-            <a href="https://tailwindcss.com/" target="_blank">
+            <a
+              href="https://tailwindcss.com/"
+              target="_blank"
+              className="underline"
+            >
               Tailwind CSS
             </a>{" "}
             in Madison, Wisconsin. Inspired by{" "}
-            <a href="https://leerob.io/" target="_blank">
+            <a href="https://leerob.io/" target="_blank" className="underline">
               Lee Robinson
             </a>
             ,{" "}
-            <a href="https://griko.id/" target="_blank">
+            <a href="https://griko.id/" target="_blank" className="underline">
               Griko Nibras
             </a>
             , and{" "}
-            <a href="https://lowmess.com/" target="_blank">
+            <a
+              href="https://lowmess.com/"
+              target="_blank"
+              className="underline"
+            >
               Alec Lomas
             </a>
             . MIT License © 2021 James Ungaretti.
