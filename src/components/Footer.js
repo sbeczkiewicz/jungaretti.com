2e9bb6a97fbdf33da1247d06ceaea8e48a044c75
JP Ungaretti <hello@jungaretti.com>
2021-02-21 17:00:08 -0600

    Fix footer spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,37 +11,41 @@ function FooterGroup({ title, children }) {
 
 export default function Footer() {
   return (
-    <footer className="container pb-6">
+    <div className="container">
       <hr />
-      <div className="mt-6 flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
-        <FooterGroup title="Here">
-          <Link title="Home" href="/" />
-        </FooterGroup>
-        <FooterGroup title="There">
-          <Link
-            title="GitHub"
-            href="https://github.com/jungaretti/"
-            isExternal
-          />
-        </FooterGroup>
-        <FooterGroup>
-          <p>
-            Crafted with{" "}
-            <Link title="Next.js" href="https://nextjs.org/" isExternal /> and{" "}
+      <footer className="py-6">
+        <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-6">
+          <FooterGroup title="Here">
+            <Link title="Home" href="/" />
+          </FooterGroup>
+          <FooterGroup title="There">
             <Link
-              title="Tailwind CSS"
-              href="https://tailwindcss.com/"
+              title="GitHub"
+              href="https://github.com/jungaretti/"
               isExternal
-            />{" "}
-            in Madison, Wisconsin. Inspired by{" "}
-            <Link title="Lee Robinson" href="https://leerob.io/" isExternal />,{" "}
-            <Link title="Griko Nibras" href="https://griko.id/" isExternal />,
-            and{" "}
-            <Link title="Alec Lomas" href="https://lowmess.com/" isExternal />.
-            MIT License © 2021 James Ungaretti.
-          </p>
-        </FooterGroup>
-      </div>
-    </footer>
+            />
+          </FooterGroup>
+          <FooterGroup>
+            <p>
+              Crafted with{" "}
+              <Link title="Next.js" href="https://nextjs.org/" isExternal /> and{" "}
+              <Link
+                title="Tailwind CSS"
+                href="https://tailwindcss.com/"
+                isExternal
+              />{" "}
+              in Madison, Wisconsin. Inspired by{" "}
+              <Link title="Lee Robinson" href="https://leerob.io/" isExternal />
+              ,{" "}
+              <Link title="Griko Nibras" href="https://griko.id/" isExternal />,
+              and{" "}
+              <Link title="Alec Lomas" href="https://lowmess.com/" isExternal />
+              .
+            </p>
+            <p className="mt-2">MIT License © 2021 James Ungaretti.</p>
+          </FooterGroup>
+        </div>
+      </footer>
+    </div>
   );
 }
