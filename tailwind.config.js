26119bcf12bf4f24734b61504ed4b25383b61f75
JP Ungaretti <hello@jungaretti.com>
2021-02-28 16:24:51 -0600

    Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        "theme-dark": "#2F3137",
+        "theme-text": "#FDFDFD",
+      },
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
