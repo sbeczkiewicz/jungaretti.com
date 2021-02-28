d82c9da67811ef3013486a42926257fa8dea6399
JP Ungaretti <hello@jungaretti.com>
2021-02-28 16:29:24 -0600

    Adjust header margin

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header>
-      <div className="container flex my-8">
+    <header className="my-8">
+      <div className="container flex">
         <div className="flex-auto">
           <span className="flex-auto font-bold">JP Ungaretti</span>
         </div>
