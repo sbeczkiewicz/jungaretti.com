f99125f7b952e1f6bb1cdae56f5b96aa2c83c64e
JP Ungaretti <hello@jungaretti.com>
2021-02-28 23:32:48 -0600

    Update welcome banner text size

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,8 +26,8 @@ export default function Home() {
       <main>
         <section className="bg-gradient-to-r from-theme-viridian to-theme-steel">
           <div className="container py-8 sm:py-12">
-            <h1 className="text-3xl sm:text-5xl font-bold">Ciao, I'm JP</h1>
-            <p className="font-medium mt-2">
+            <h1 className="text-4xl sm:text-5xl font-bold">Ciao, I'm JP</h1>
+            <p className="text-lg sm:text-xl font-medium mt-2">
               I'm a developer studying computer science and Italian. I love to
               swim, play board games, mess around with computers, and visit
               national parks. Welcome to my corner of the Internet.
