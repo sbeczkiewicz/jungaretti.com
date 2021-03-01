1cd075a486bd5a38aefb6ff090af9abd7526ec8b
JP Ungaretti <hello@jungaretti.com>
2021-02-28 23:58:35 -0600

    Remove small margin

--- src/components/DetailHeader.js
+++ src/components/DetailHeader.js
@@ -2,7 +2,7 @@ export default function DetailHeader({ title, subtitle }) {
   return (
     <div>
       <h6 className="text-sm font-bold opacity-80 uppercase">{subtitle}</h6>
-      <h2 className="text-2xl sm:text-4xl font-bold mt-2">{title}</h2>
+      <h2 className="text-2xl sm:text-4xl font-bold">{title}</h2>
     </div>
   );
 }
