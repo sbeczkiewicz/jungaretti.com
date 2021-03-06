50b803fe01e7ca5b4ef4eb981030eeb5b6a8f0f2
JP Ungaretti <hello@jungaretti.com>
2021-03-05 19:52:37 -0600

    Update footer text

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,7 +47,7 @@ export async function getStaticProps(context) {
         "My corner of the Internet. Built with Next.js and Tailwind CSS. Thanks for checking it out!",
       links: [
         {
-          title: "Repository",
+          title: "GitHub",
           href: "https://github.com/jungaretti/jungaretti.com",
         },
         {
@@ -62,7 +62,7 @@ export async function getStaticProps(context) {
         "Highly-functional milk tracker with a potential userbase  of 5.6 million Wisconsinites. Built with React, Firebase, and Steve Beczkiewicz during a weekend hackathon.",
       links: [
         {
-          title: "Repository",
+          title: "GitHub",
           href: "https://github.com/jungaretti/milk-chugger",
         },
         {
@@ -77,7 +77,7 @@ export async function getStaticProps(context) {
         "Elegant and functional code snippets for the web. Built with Express, Pug, and Azure during Microsoft's annual Hackathon.",
       links: [
         {
-          title: "Repository",
+          title: "GitHub",
           href: "https://github.com/jungaretti/digist",
         },
       ],
