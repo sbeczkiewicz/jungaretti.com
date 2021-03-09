e7f2f96922e1ffb06ec847ef345bf1fcb4f347bd
JP Ungaretti <hello@jungaretti.com>
2021-03-08 23:59:16 -0600

    Add project link separators

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,15 +3,18 @@ export default function Project({ title, description, links }) {
     <div>
       <h3 className="text-lg font-bold">{title}</h3>
       <p>{description}</p>
-      <ul className="flex flex-row space-x-4">
-        {links.map(({ title, href }, key) => (
-          <li key={key}>
-            <span className="text-theme-griko underline">
-              <a href={href} target="_blank">
-                {title}
-              </a>
-            </span>
-          </li>
+      <ul className="flex flex-row space-x-2">
+        {links.map(({ title, href }, key, array) => (
+          <>
+            <li key={key}>
+              <span className="text-theme-griko">
+                <a href={href} target="_blank">
+                  {title}
+                </a>
+              </span>
+            </li>
+            {key != array.length - 1 ? <span>·</span> : null}
+          </>
         ))}
       </ul>
     </div>
