ec8661993d22604f7cfb2a036be85a6c63abb0de
JP Ungaretti <hello@jungaretti.com>
2021-03-14 19:39:41 -0500

    Move projects to their own file

---
+++ data/projects.js
@@ -0,0 +1,39 @@
+export let projects = [
+  {
+    title: "jungaretti.com",
+    description:
+      "My corner of the Internet. Built with Next.js and Tailwind CSS. Thanks for checking it out!",
+    links: [
+      {
+        title: "Repository",
+        href: "https://github.com/jungaretti/jungaretti.com",
+      },
+    ],
+  },
+  {
+    title: "Milk Chugger",
+    description:
+      "Highly-functional milk tracker with a potential userbase  of 5.6 million Wisconsinites. Built with React, Firebase, and Steve Beczkiewicz during a weekend hackathon.",
+    links: [
+      {
+        title: "Repository",
+        href: "https://github.com/jungaretti/milk-chugger",
+      },
+      {
+        title: "Steve's Website",
+        href: "https://sbeczkiewicz.github.io/",
+      },
+    ],
+  },
+  {
+    title: "Digist",
+    description:
+      "Elegant and functional code snippets for the web. Built with Express, Pug, and Azure during Microsoft's annual Hackathon.",
+    links: [
+      {
+        title: "Repository",
+        href: "https://github.com/jungaretti/digist",
+      },
+    ],
+  },
+];
